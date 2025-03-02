1c11e9948ae4f70b2a0868bf9352d62cadf460cb
2023_ljd <2217322483@qq.com>
2025-03-02 16:13:55 +0800

    submitted
    none

---
+++ DES-master/des.h
@@ -0,0 +1,20 @@
+#ifndef _DES_H_
+#define _DES_H_
+
+#define ENCRYPTION_MODE 1
+#define DECRYPTION_MODE 0
+
+typedef struct {
+	unsigned char k[8];
+	unsigned char c[4];
+	unsigned char d[4];
+} key_set;
+
+void generate_key(unsigned char* key);
+void generate_sub_keys(unsigned char* main_key, key_set* key_sets);
+void process_message(unsigned char* message_piece, unsigned char* processed_piece, key_set* key_sets, int mode);
+
+//新添加的函数get_message,用于获取指定message
+unsigned char get_message(int i , int j , char l) ;
+
+#endif
