be168325012c1a63917d3b62360eea22f1ce9aa3
2023ljd <151378479+2023ljd@users.noreply.github.com>
2025-03-02 17:02:53 +0800

    Delete run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-echo "# DES-lab-report" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/2023ljd/DES-lab-report.git
-git push -u origin main
